ca8e5bc14a7842bb8af8a4424f9a201770beaf91
Tyler <tyler@eeosk.com>
2017-03-18 17:17:55 -0700

    Added additional pages

--- server.js
+++ server.js
@@ -3,9 +3,21 @@
 let express     = require('express')
 ,   serveStatic = require('serve-static')
 ,   path        = require('path')
-,   ejs         = require('ejs')
 ,   app = express()
 ,   port = process.env.PORT || 3000
+
+const routes = [
+  { path: '/',          file: 'index.html' },
+  { path: '/issues',    file: 'issues.html' },
+  { path: '/resources', file: 'resources.html' },
+  { path: '/contact',   file: 'contact.html' },
+]
+
+routes.map(pair => {
+  app.get(pair.path, (req, res, next) => {
+    res.sendFile(pair.file, { root: 'dist' })
+  })
+})
 
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
